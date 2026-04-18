d46117bff3fa220311a32fdc5df43eed31cd5e92
HyperPigeon <58276133+HyperPigeon@users.noreply.github.com>
2026-04-18 13:47:02 -0400

    - added SeriesReading.java - added external function to act reading that calls when knot is visited - added KnotListener.java

---
+++ src/main/java/symbolics/division/renmi/story/KnotListener.java
@@ -0,0 +1,5 @@
+package symbolics.division.renmi.story;
+
+public interface KnotListener {
+	void onKnotVisited(String knot);
+}
--- src/main/java/symbolics/division/renmi/story/SeriesReading.java
+++ src/main/java/symbolics/division/renmi/story/SeriesReading.java
@@ -1,5 +1,59 @@
 package symbolics.division.renmi.story;
 
-public class SeriesReading {
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.core.UUIDUtil;
+import net.minecraft.resources.Identifier;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class SeriesReading implements KnotListener {
+
+	public static Codec<SeriesReading> CODEC = RecordCodecBuilder.create(i -> i.group(
+		Codec.unboundedMap(Identifier.CODEC, ActReading.CODEC).fieldOf("actReadings").forGetter(ser -> ser.actReadings),
+		Codec.unboundedMap(Codec.STRING, Codec.BOOL).fieldOf("knotsVisited").forGetter(ser -> ser.knotsVisited),
+		ActReading.CODEC
+			.fieldOf("currentActReading")
+			.forGetter(SeriesReading::getCurrentActReading)
+		).apply(i, SeriesReading::new)
+	);
+
+	protected final Map<Identifier, ActReading> actReadings;
+	protected final Map<String, Boolean> knotsVisited;
+	protected ActReading currentActReading;
+
+
+	public SeriesReading(){
+		this.actReadings = new HashMap<>();
+		this.knotsVisited = new HashMap<>();
+		this.currentActReading = null;
+	}
+
+
+	public SeriesReading(Map<Identifier, ActReading> actReadings, Map<String, Boolean> knotsVisited, ActReading currentActReading){
+		this.actReadings = actReadings;
+		this.knotsVisited = knotsVisited;
+		this.currentActReading = currentActReading;
+	}
+
+	public void setCurrentActReading(ActReading actReading) {
+		this.currentActReading = actReading;
+	}
+
+
+	public ActReading getCurrentActReading() {
+		return currentActReading;
+	}
+
+
+	public boolean isKnotVisited(String knot) {
+		return this.knotsVisited.getOrDefault(knot, false);
+	}
+
+	@Override
+	public void onKnotVisited(String knot) {
+		this.knotsVisited.put(knot,true);
+	}
 }
