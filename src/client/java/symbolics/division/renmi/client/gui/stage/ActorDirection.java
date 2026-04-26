e3a48aa0eb1de8d8298e9e2c7b334236865330f8
Chai <chai@noreply.chailotl.dev>
2026-04-26 18:58:28 -0400

    Syntax cleanup

--- src/client/java/symbolics/division/renmi/client/gui/stage/ActorDirection.java
+++ src/client/java/symbolics/division/renmi/client/gui/stage/ActorDirection.java
@@ -2,9 +2,9 @@ package symbolics.division.renmi.client.gui.stage;
 
 import net.minecraft.resources.Identifier;
 
-public record ActorDirection(Identifier id, String expression, int posX, int posY, int slotPos,
-                             boolean faceRight) implements StageDirection {
-
+public record ActorDirection(
+	Identifier id, String expression, int posX, int posY, int slotPos, boolean faceRight
+) implements StageDirection {
 	public Identifier getTexture() {
 		return Identifier.fromNamespaceAndPath(
 			id.getNamespace(),
